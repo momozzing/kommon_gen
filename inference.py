370195b83d14bad0b01d02687297d4802c40d9c6
momozzing <ahdbsgh2@naver.com>
2022-05-06 17:53:34 +0900

    test

--- inference.py
+++ inference.py
@@ -1,8 +1,12 @@
 """
 python interactive.py
 """
+from argparse import ArgumentParser
 import torch
 from transformers import AutoModelWithLMHead, AutoTokenizer
+import pandas as pd
+from tqdm import tqdm
+
 
 model_name = "skt/kogpt2-base-v2"
 ckpt = 'skt-kogpt2-base-v2-5.pt'
@@ -11,13 +15,13 @@ ckpt_name = f"model_save/{ckpt}"
 model = AutoModelWithLMHead.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-
 SPECIAL_TOKENS = {
     "bos_token": "[bos]",
     "eos_token": "[eos]",
     "pad_token": "[pad]",
     "sep_token": "[sep]"
     }
+
 tokenizer.add_special_tokens(SPECIAL_TOKENS)
 
 model.resize_token_embeddings(len(tokenizer)) 
@@ -26,38 +30,47 @@ model.resize_token_embeddings(len(tokenizer))
 model.load_state_dict(torch.load(ckpt_name, map_location="cpu"))
 model.cuda()
 
-with torch.no_grad():
-    while True:
-        t = input("\nUser: ")
-        tokens = tokenizer(
-            t+'[sep]',
-            return_tensors="pt",
-            truncation=True,
-            padding=True,
-            max_length=30
-        )
 
-        input_ids = tokens.input_ids.cuda()
-        # print(input_ids)
-        # print(tokenizer.convert_ids_to_tokens(input_ids[0])) 
-        attention_mask = tokens.attention_mask.cuda()
-        sample_output = model.generate(
-            input_ids, 
-            max_length=30, 
-            min_length=10,
-            num_beams=10, 
-            no_repeat_ngram_size=3,
-            early_stopping=True
-        )
-        gen = sample_output[0]
-        print("System: " + tokenizer.decode(gen[len(input_ids[0]):-1], skip_special_tokens=True))
+test_data = pd.read_csv("data/Test/label_data/test_data.csv", delimiter="\t")
+test_data = test_data[:5]
+test_text, test_labels = (
+    test_data["concept_set"].values,
+    test_data["label"].values,
+)
+
+dataset = [
+    {"data": t , "label": l }
+    for t, l in zip(test_text, test_labels)
+]
 
 
+gen_text = []
 
-#     en_label.append(str(label))
-#     gen_label.append(str(tokenizer.decode(sample_output[0], skip_special_tokens=True)))
+for data in tqdm(dataset):
+    text, label = data["data"], data["label"]
+    tokens = tokenizer(
+        text + '[sep]',
+        return_tensors="pt",
+        truncation=True,
+        padding=True,
+        max_length=30
+    )
 
-# label_df = pd.DataFrame(en_label, columns = ['label'])
-# gen_df = pd.DataFrame(gen_label, columns = ['gen'])
-# all_df = pd.concat([label_df, gen_df], axis=1)
-# all_df.to_csv(f'result/gen_text_{ckpt}.csv', sep='\t')
+    input_ids = tokens.input_ids.cuda()
+    attention_mask = tokens.attention_mask.cuda()
+
+    sample_output = model.generate(
+        input_ids, 
+        max_length=30, 
+        min_length=10,
+        num_beams=10, 
+        no_repeat_ngram_size=3,
+        # early_stopping=True
+    )
+    gen = sample_output[0]
+    gen_text.append(str(tokenizer.decode(gen[len(input_ids[0]):-1], skip_special_tokens=True)))
+
+
+
+gen_df = pd.DataFrame(gen_text, columns = ['gen'])
+gen_df.to_csv(f'result/gen_text_{ckpt}.csv', sep='\t')
